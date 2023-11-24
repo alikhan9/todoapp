ee234d51a8d932013e3dac639f237f5626c40806
alikhan9 <alikhan.zaipoulaiev@outlook.fr>
2023-11-24 12:55:49 +0100

    fix profile edit scroll

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -17,7 +17,7 @@ defineProps({
 
 <template>
     <Head title="Profile" />
-    <div class="lg:max-h-[85vh]  max-h-[87vh]">
+    <div class="lg:max-h-[85vh]  md:max-h-[87vh]">
         <div>
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6 sm:mt-2 lg:mt-6 overflow-auto h-full md:max-h-[85vh]">
                 <div class="p-4 sm:p-6 bg-[hsl(0,0%,15%)] text-white border-2 rounded-xl border-[hsl(0,0%,25%)]">
